33a6a8997cc9fa6679010de38768ea61493b159a
Docvel <docvel@yandex.ru>
2023-03-21 02:57:47 +0300

    added gitignore, *.csv and tasks

---
+++ task40.py
@@ -0,0 +1,5 @@
+# Работать с файлом california_housing_train.csv. Определить среднюю стоимость
+# дома, где кол-во людей от 0 до 500 (population).
+import pandas as pnd
+
+main_csv = pnd.read_csv("california_housing_test.csv")
---
+++ task42.py
@@ -0,0 +1,4 @@
+# Узнать какая максимальная households в зоне минимального значения population.
+import pandas as pnd
+
+main_csv = pnd.read_csv("california_housing_test.csv")
