44e31940a5c26b6498db5542d48c6186b660f15a
Docvel <docvel@yandex.ru>
2023-03-21 03:51:48 +0300

    task 40 is solved

--- task40.py
+++ task40.py
@@ -3,3 +3,7 @@
 import pandas as pnd
 
 main_csv = pnd.read_csv("california_housing_test.csv")
+ppl = main_csv['population']
+mhv = main_csv['median_house_value']
+print(mhv[(ppl > 0) & (ppl < 500)].mean())  # средняя арифметическая стоимость
+print(mhv[(ppl > 0) & (ppl < 500)].median())  # срединное значение
