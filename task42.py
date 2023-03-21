50f50a23be32b1cf50aa9d1a37782de24637fe47
Docvel <docvel@yandex.ru>
2023-03-21 04:16:41 +0300

    task 42 is solved

--- task42.py
+++ task42.py
@@ -2,3 +2,6 @@
 import pandas as pnd
 
 main_csv = pnd.read_csv("california_housing_test.csv")
+ppl = main_csv['population']
+hhs = main_csv['households']
+print(hhs[ppl == ppl.min()].max())  # во всей таблице такая строка одна
