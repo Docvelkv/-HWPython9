a9862315a48b1bbf8827e5cc1da33e3f4b6b0464
Docvel <docvel@yandex.ru>
2023-03-21 18:27:59 +0300

    fixed task 42

--- task42.py
+++ task42.py
@@ -5,3 +5,4 @@ main_csv = pnd.read_csv("california_housing_test.csv")
 ppl = main_csv['population']
 hhs = main_csv['households']
 print(hhs[ppl == ppl.min()].max())  # во всей таблице такая строка одна
+print(hhs[ppl == ppl.min()])  # искать максимальное значение не обязательно
